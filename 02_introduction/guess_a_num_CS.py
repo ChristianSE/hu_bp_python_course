4586d610b9788f8b13a391f5b8bc8b7d6579a8c7
PoolUser <biostudent200@fredda.biologie.hu-berlin.de>
2015-05-20 15:27:58 +0200

    adds guess a number game

--- 02_introduction/guess_a_num_CS.py
+++ 02_introduction/guess_a_num_CS.py
@@ -1,1 +1,13 @@
-Hallo... note
+import random
+print 'Guess the right number between 0 to 100 !'
+n = random.randint(0,100)
+g = int(raw_input('Please enter your 1. guess:'))
+if g == n:
+	print 'Congrats!'
+# while g != n:
+
+#	g = int(raw_input('Please enter your guess:'))
+	if g < n:
+		print 'Too low'
+	elif g > n:
+		print 'Too high'
